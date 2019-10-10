65e17f92f892f4ea68c10f34e2e545489e90ebdc
ChillarAnand <anand21nanda@gmail.com>
2019-10-10 15:29:13 +0530

    Check if zsh is installed

--- mac/bootstrap_terminal.sh
+++ mac/bootstrap_terminal.sh
@@ -3,7 +3,9 @@
 set -x
 
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+if [ ! -d ~/.oh-my-zsh/ ]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
 
 
 if [ ! -d ~/.zsh/zsh-autosuggestions/ ]; then
