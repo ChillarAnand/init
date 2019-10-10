cd6f460b6db88f0d7ca0e2402ff1192bd8462127
ChillarAnand <anand21nanda@gmail.com>
2019-10-10 15:21:04 +0530

    Check if brew is already installed

--- mac/setup.sh
+++ mac/setup.sh
@@ -6,8 +6,10 @@ set -x
 xcode-select --install
 
 
-# install brew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+if [ ! -f /usr/local/bin/brew ]; then
+    # install brew
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 
 
 project_name='platform'
