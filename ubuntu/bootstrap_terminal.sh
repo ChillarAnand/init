cff9bf28eb0aa52ceb2b41bdd91153f37dc25b28
ChillarAnand <anand21nanda@gmail.com>
2021-12-06 10:19:16 +0530

    Added ohmyzsh to bootstrap script

--- ubuntu/bootstrap_terminal.sh
+++ ubuntu/bootstrap_terminal.sh
@@ -6,4 +6,9 @@ set -x
 sudo apt update
 sudo apt install --yes zsh byobu tree git
 
+
+# oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
 byobu-enable
