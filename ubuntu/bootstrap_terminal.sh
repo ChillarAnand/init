8f293aca31007589c79c31128e994fc43651f265
chillaranand <anand21nanda@gmail.com>
2022-11-09 09:08:53 +0530

    Updated terminal bootstrap script

--- ubuntu/bootstrap_terminal.sh
+++ ubuntu/bootstrap_terminal.sh
@@ -4,11 +4,15 @@
 set -x
 
 sudo apt update
-sudo apt install --yes zsh byobu tree git nmap telnet
-
+sudo apt install --yes zsh byobu tree git nmap telnet unzip
 
 # oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 
-byobu-enable
+sudo apt install --yes python3 python3-pip
+
+pip install pyflash
+
+
+# byobu-enable
