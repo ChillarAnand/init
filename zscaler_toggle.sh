043d28a030a5b2c253d48403ecf06514fb721dd4
ChillarAnand <chillar@avilpage.com>
2023-09-21 17:37:38 +0530

    Update zscaler script

--- zscaler_toggle.sh
+++ zscaler_toggle.sh
@@ -9,8 +9,10 @@ then
     find /Library/LaunchAgents -name '*zscaler*' -exec launchctl unload {} \;
     sudo find /Library/LaunchDaemons -name '*zscaler*' -exec launchctl unload {} \;
 else
+    pgrep 'Google Chrome' | xargs kill -9
+    pgrep Tailscale | xargs kill -9
     echo "Starting Zscaler"
-    pkill -f 'Google Chrome'
+    # pkill -f 'Google Chrome'
     open -a /Applications/Zscaler/Zscaler.app --hide
     sudo find /Library/LaunchDaemons -name '*zscaler*' -exec launchctl load {} \;
 fi
